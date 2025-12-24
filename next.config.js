d9fc99235ca4ccf1f440da82842e1907a2ad20df
danish7x7 <danishbirsingh.bhatti@sjsu.edu>
2025-12-24 12:44:40 -0800

    added workflow

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: process.env.NODE_ENV === 'production' ? '/danish7x7.github.io' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/danish7x7.github.io/' : '',
-    trailingSlash: true,
-  }
-  
-  module.exports = nextConfig
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '',
+  assetPrefix: '',
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
